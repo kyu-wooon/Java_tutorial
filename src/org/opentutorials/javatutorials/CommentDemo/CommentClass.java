6b3424a037ea4d2bddd84db36c2dccef3d9b3589
kyu-wooon <catdog0831@gmail.com>
2023-01-09 17:16:23 +0900

    23/01/09 주석공부

---
+++ src/org/opentutorials/javatutorials/CommentDemo/CommentClass.java
@@ -0,0 +1,25 @@
+package org.opentutorials.javatutorials.CommentDemo;
+
+public class CommentClass {
+    public static void main(String[] args) {
+        // 두개의 변수가 같은 데이터 타입 일 때 아래와 같이 코드를 작성한다.
+        String a, b;
+        /*
+        a = "coding";
+        b = "everybody";
+        System.out.println(a+b);
+        */
+        // assignment statement
+        double aValue;
+        aValue = 8933.234;
+        // increment statement
+        aValue++;
+        // method invocation statement
+        System.out.println("Hello World!");
+        // object creation statement
+
+        //Bicycle myBike = new Bicycle();
+        int c = 100; double d = 10.1;
+    }
+
+}
