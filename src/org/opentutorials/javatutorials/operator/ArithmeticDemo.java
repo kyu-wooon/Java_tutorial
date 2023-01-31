bfaa605f3927d558657e89a39a4b53b9aed0d312
kyu-wooon <catdog0831@gmail.com>
2023-01-31 15:36:26 +0900

    23/01/30~31 연산공부

---
+++ src/org/opentutorials/javatutorials/operator/ArithmeticDemo.java
@@ -0,0 +1,35 @@
+package org.opentutorials.javatutorials.operator;
+
+public class ArithmeticDemo {
+    public static void main(String[] args) {
+        //result 값은 3
+        int result = 1 + 2; // + 기호 : 더하기
+        System.out.println(result);
+
+        //result 값은 2
+        result = result - 1; // - 기호 : 빼기
+        System.out.println(result);
+
+        //result 값은 4
+        result = result * 2; // * 기호 : 곱하기
+        System.out.println(result);
+
+        //result 값은 2
+        result = result / 2; // / 기호 : 나누기
+        System.out.println(result);
+
+        //result의 값은 10
+        result = result + 8;
+        System.out.println(result);
+
+        //result의 값은 3
+        result = result %  7; // % 기호 : n 값으로 나눈 나머지
+        System.out.println(result);
+
+
+
+
+
+
+    }
+}
